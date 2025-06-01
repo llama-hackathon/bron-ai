a36e601da0e562a57473d3a910e6824d3b5ca3e4
gnano222 <jgnanamithran@gmail.com>
2025-06-01 08:30:46 -0400

    consolidated annotator into talk2Video. and added referee

--- core/referree.py
+++ core/referree.py
@@ -1,19 +1,161 @@
 import os
 import sys
-
+import json
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils.video import parse_foul_info
+from core.talk2Video import Talk2Video
+
+DETAIL_ANNOTATOR_PROMPT = """
+        You are a keen eyed basketball referee, watching a basketball play. 
+    
+        Analyze the provided play (represented by a sequence of basketball image frames) for detail relevant for foul calling, especially any contact. If there is contact, you sohuld
+        describe it in detail - what body part was hit, what contact type was used, how severe the contact was. Since the frames comprise a single play, you should interpret the play in the
+        context *all* the provided images. A single image might not provide enough information. Each frame is 50 milliseconds apart!
+
+        Your job is not to call fouls, but to describe the action in detail for someone else to call fouls.
+        
+        We are only concerned with the ball handler/shooter and the primary defender.
+
+        The overall context of the sequence is known to be a basketball game, so you can focus on the actions of the ball handler and the primary defender.
+
+        Do not try to identify the players or teams, just refer to them as players on offense/defense, etc.
+
+        You do not need to give a frame by frame description, just the most relevant information of the sequence. 
+
+    """
+
+FOUL_JUDGEMENT_PROMPT = """ 
+        You are a seasoned basketball referee with expert knowledge of NBA rules
+        for defensive fouls on a shooter.
+
+        **Your job**
+
+        Given a time-ordered list of annotations of a single basketball play, 
+        decide whether the **primary defender commits a defensive
+        shooting foul** during the shot attempt described.
+
+        Despite being so information dense, these annotations still correspond to a single basketball play taking place over a couple of seconds! it is all one moment to be taken in at once, and the 
+        amount of information is provided in order to give you enough minute information to make your decision: to call foul or not foul.
+
+        <IMPORTANT>
+        You can return free form text, but ensure you surround your final answer with backticks. either ```FOUL``` or ```CLEAN```
+        </IMPORTANT>
+        
+         Look for the following foul indicators _during the shooting motion:
+
+        • Illegal contact with the shooter's arms, wrist, or hand on the ball  
+        • Body-to-body displacement that affects balance or verticality  
+        • Defender invading the shooter's landing space (counter to rule 10-IV-f)  
+        • Contact on the head/neck or airborne shooter (automatic)  
+        • Push from behind or on the side causing altered shot trajectory
+
+
+        annotation data:
+
+        ANNOTATION_DATA_TOKEN"""
 
 class Referee:
     def __init__(self):
         pass
 
-    def look_into_video(self, video_filepath: str):
+    def look_into_video(self, video_filepath: str, timestamp: int, boundry_seconds: int = 2):
         """
         Look into a video file and extract frames for annotation.
         """
+        talk_to_video = Talk2Video(video_filepath)
 
-        # 
-        vid = parse_foul_info(video_filepath)
+        # focus on frames around the timestamp
+        frames = talk_to_video.vid.cut_frames(max(timestamp-boundry_seconds, 0), timestamp + boundry_seconds, seconds_per_frame=0.2)
 
+        keys = list(frames.keys())
+        keys.sort(key=float)
 
+        groups = [keys[i:i + 7] for i in range(0, len(keys), 6)]
+
+        analyses = []
+        for group in groups:
+            messages = []
+            messages.append({
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": DETAIL_ANNOTATOR_PROMPT,
+                    },
+                ]
+            })
+            for k in group:
+                messages.append({
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{frames[k]['data']}"
+                            },
+                        },
+                    ]
+                })
+            resp = talk_to_video.vid.client.chat.completions.create(
+                model="Llama-4-Maverick-17B-128E-Instruct-FP8",
+                messages=messages,
+            )
+            analyses.append(resp.completion_message.content.text)
+
+        print("\n--------------------------------\n".join(analyses))
+        print('--------------------------------')
+        judgement = talk_to_video.vid.client.chat.completions.create(
+            model="Llama-4-Maverick-17B-128E-Instruct-FP8",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": FOUL_JUDGEMENT_PROMPT.replace("ANNOTATION_DATA_TOKEN", "\n".join(analyses)),
+                        }
+                    ]
+                }
+            ],
+        )
+
+        judge_text = judgement.completion_message.content.text
+        print(judge_text)
+
+        
+        if "```FOUL```" in judge_text:
+            is_foul_present =  True
+        elif "```CLEAN```" in judge_text:
+            is_foul_present = False
+        else:
+            print(f"Invalid response!!!!!: {judge_text}")
+            is_foul_present = False
+
+        clumped_annotations = "\n----\n".join(analyses)
+        json_annotations = json.dumps(clumped_annotations)
+        json_judgement = json.dumps(judge_text)
+
+        return json_annotations, json_judgement, is_foul_present
+    
+    def make_decision(self, json_annotations: str, json_judgement: str):
+        """
+        Use finetuned LLM Model to make a decision based on the annotations and judgement text.
+        """
+        pass
+    
+if __name__ == "__main__":
+    base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+    video_file = os.path.join(base_dir, 'data', 'video', 'game_2_20.mp4')
+    # video_file = os.path.join(base_dir, 'data', 'foulless_play.mp4')
+
+    ref = Referee()
+
+    res = []
+    for interesting_ts in [22, 152, 177, 217, 262, 337, 477, 582, 22, 152, 177, 217, 262, 272, 337, 362, 477, 582, 637, 647, 672, 767, 817, 927, 937, 1127, 1132, 1177, 1187]:
+        json_escaped_annotations, judgement_text, is_foul_present = ref.look_into_video(video_file, interesting_ts, boundry_seconds=2)
+        res.append((interesting_ts, json_escaped_annotations, judgement_text, is_foul_present))
+
+    # pipe separated csv
+    # import csv
+    # with open('res.csv', 'w') as f:
+    #     writer = csv.writer(f, delimiter='|')
+    #     writer.writerows(res)
--- core/talk2Video.py
+++ core/talk2Video.py
@@ -4,29 +4,99 @@ import sys
 import time
 import numpy as np
 import concurrent.futures
-
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from utils.llama_api import LlamaAPI
+from utils.video import Video
+from utils.audio import Audio
 
 
 class Talk2Video:
-    def __init__(self, annotations_path: str):
-        self.annotations_path = annotations_path
-        self.annotations = self.load_annotations()
+    def __init__(self, video_filepath: str):
+        self.video_filepath = video_filepath
+        self.vid = Video(self.video_filepath)
         self.llama_api = LlamaAPI()
-
-        # Create a smaller version of self.annotations: {timestamp: annotation}
-        self.simple_annotations = {str(k): v.get("annotation", "") for k, v in self.annotations.items()}
     
-    def load_annotations(self) -> dict:
+    def annotate_video(self, seconds_per_frame: int = 1, context: str = None):
+        """Annotates a video file."""
+
+        # 1. Extract frames at desired sampling rate
+        frame_dict = self.vid.extract_frames(seconds_per_frame=seconds_per_frame)
+
+        # 2. Describe frames
+        frames = self.vid.describe_frames(
+            frame_dict,
+            context = context,
+            threads=10)
+        self.save_annotations(frames, os.path.join("data", "annotations", f"{self.vid.name_no_ext}_annotations_vid.json"))
+
+    def annotate_audio(self):
+
+        # 1. Extract audio
+        audio_output = os.path.join(self.video_filepath, '..', 'audio', f'{self.vid.name_no_ext}.wav')
+        self.vid.extract_audio(audio_output)
+
+        # 2. Transcribe audio
+        audio = Audio(audio_output)
+        transcription = audio.transcribe()
+        transcription_segments = transcription.get('segments', [])
+        frames = {}
+
+        for segment in transcription_segments:
+            start = segment['start']
+            end = segment['end']
+            middle = (start + end) / 2
+            # add annotation to frames dict
+            frames[middle] = {
+                'source': f'{self.vid.name_no_ext}.wav',
+                'source_type': 'audio',
+                'annotation': segment.get('text', ''),
+                'start': start,
+                'end': end
+            }
+
+        # Save to a JSON file
+        self.save_annotations(frames, os.path.join("data", "annotations", f"{self.vid.name_no_ext}_annotations_audio.json"))
+
+    def compile_annotaions(self, video_annotations, audio_annotations):
+        """Compiles video and audio annotations into a single dictionary."""
+        compiled_annotations = {}
+        for timestamp, annotation in video_annotations.items():
+            # Exclude 'data' key from annotation if present
+            annotation_no_data = {k: v for k, v in annotation.items() if k != 'data'}
+            compiled_annotations[timestamp] = annotation_no_data
+
+        for timestamp, annotation in audio_annotations.items():
+            if timestamp not in compiled_annotations:
+                compiled_annotations[timestamp] = annotation
+            else:
+                # If both video and audio annotations exist at the same timestamp, merge them
+                compiled_annotations[timestamp].update(annotation)
+
+        self.save_annotations(compiled_annotations, os.path.join("data", "annotations", f"{self.vid.name_no_ext}_annotations.json"))
+
+    def save_annotations(self, annotations, output_path):
+        """Saves the annotations to a JSON file."""
+        with open(output_path, "w") as f:
+            json.dump(annotations, f, indent=2)
+        self.annotations = dict(sorted(annotations.items(), key=lambda item: float(item[0])))
+        print(f"Frame metadata saved to {output_path}")
+
+    def load_annotations(self, annotations_path: str) -> dict:
         """Load annotations from a JSON file."""
+        self.annotations_path = annotations_path
         if not os.path.exists(self.annotations_path):
             raise FileNotFoundError(f"Annotations file not found: {self.annotations_path}")
         
         with open(self.annotations_path, 'r') as f:
-            return dict(sorted(json.load(f).items(), key=lambda item: float(item[0])))
+            annot =  dict(sorted(json.load(f).items(), key=lambda item: float(item[0])))
+            self.annotations = annot
+            # Create a smaller version of self.annotations: {timestamp: annotation}
+        self.simple_annotations = {str(k): v.get("annotation", "") for k, v in self.annotations.items()}
+        return annot
     
-
     def chunk_annotations(self, annotations:dict, token_limit: int = 128000) -> list:
         """
         Splits a dictionary into a list of dictionaries, each not exceeding the token_limit
@@ -99,7 +169,6 @@ class Talk2Video:
         response = self.llama_api.ask(final_messages, model='Llama-4-Maverick-17B-128E-Instruct-FP8')
         return aggregated_summary
         
-
     def look_for_event(self, event: str, window_length:int = 5, search_start:int=0, search_end=np.inf) -> list:
         """
         Compiles annotations within a specified window range.
@@ -112,7 +181,6 @@ class Talk2Video:
         event_timestamps = []
 
         # Create windows by looping through the timestamps
-
         def search_window(args):
             start, window_length, event, simple_annotations = args
             end = start + window_length
@@ -160,26 +228,43 @@ class Talk2Video:
 
 if __name__ == "__main__":
     base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
-    annotations_file = os.path.join(base_dir, 'data', 'annotations', 'game_2_20_annotations.json')
-    
-    talk2video = Talk2Video(annotations_file)
+    video_path = os.path.join(base_dir, 'data', 'video', 'game_2_20.mp4')
+    talk2video = Talk2Video(video_path)
 
+    ##### ANNOTATE VIDEO #####
+    # talk2video.annotate_video(
+    #     seconds_per_frame=60, 
+    #     context=("This is a frame of tv footage of a basketball game, you don't need to mention that in your response. "
+    #         "If the frame is of the basketball game in play and you can see the ball, only focus on the ball handler, what he is doing and how he is being defended."
+    #         "Be specific with details a referee would be interested in. "
+    #         "Assume that frames a second ago (and beyond) have already been annotated, so you can focus on the subject at hand. No need to 'start from scratch' in describing the game."
+    #         "Ignore descriptions of the setting, stadium, crowd or the scoreboard on screen"))
+    
+    # talk2video.annotate_audio()
+
+    # talk2video.compile_annotaions(
+    #     video_annotations=json.load(open(os.path.join("data", "annotations", f"{talk2video.vid.name_no_ext}_annotations_vid.json"), 'r')),
+    #     audio_annotations=json.load(open(os.path.join("data", "annotations", f"{talk2video.vid.name_no_ext}_annotations_audio.json"), 'r'))
+    # )
     # summary = talk2video.summarize_annotations()
     # print("Video Summary:")
     # print(summary)
-    fouls = []
-    for i in range(1, 3):
-        print(i)
-        fouls_window = talk2video.look_for_event("""
-                • Illegal contact with the shooter's arms, wrist, or hand on the ball
-                • Body-to-body displacement that affects balance or verticality
-                • Defender invading the shooter's landing space (counter to rule 10-IV-f)
-                • Contact on the head/neck or airborne shooter (automatic)
-                • Push from behind or on the side causing altered shot trajectory
-        """, 
-        window_length=5,
-        search_start=0,
-        search_end=60*i*10)  # Search within the first 5 minutes
-        fouls.extend(fouls_window)
-        time.sleep(20)
-        print(fouls)
+
+    ##### LOOK FOR EVENT #####
+    # annotations_file = os.path.join(base_dir, 'data', 'annotations', 'game_2_20_annotations.json')
+    # fouls = []
+    # for i in range(1, 3):
+    #     print(i)
+    #     fouls_window = talk2video.look_for_event("""
+    #             • Illegal contact with the shooter's arms, wrist, or hand on the ball
+    #             • Body-to-body displacement that affects balance or verticality
+    #             • Defender invading the shooter's landing space (counter to rule 10-IV-f)
+    #             • Contact on the head/neck or airborne shooter (automatic)
+    #             • Push from behind or on the side causing altered shot trajectory
+    #     """, 
+    #     window_length=5,
+    #     search_start=0,
+    #     search_end=60*i*10)
+    #     fouls.extend(fouls_window)
+    #     time.sleep(20)
+    #     print(fouls)

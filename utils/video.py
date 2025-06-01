ed435afc52cce88981f1398fbdcb4243b301add2
benedictb <becker.benedictj@gmail.com>
2025-05-31 22:44:06 -0400

    wip

--- utils/video.py
+++ utils/video.py
@@ -10,11 +10,69 @@ import json
 import concurrent.futures
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
+
+DETAIL_ANNOTATOR_PROMPT = """
+        You are a keen eyed basketball referee, watching a basketball play. 
+    
+        Analyze the provided play (represented by a sequence of basketball image frames) for detail relevant for foul calling, especially any contact. If there is contact, you sohuld
+        describe it in detail - what body part was hit, what contact type was used, how severe the contact was. Since the frames comprise a single play, you should interpret the play in the
+        context *all* the provided images. A single image might not provide enough information. Each frame is 50 milliseconds apart!
+
+        Your job is not to call fouls, but to describe the action in detail for someone else to call fouls.
+        
+        We are only concerned with the ball handler/shooter and the primary defender.
+
+        The overall context of the sequence is known to be a basketball game, so you can focus on the actions of the ball handler and the primary defender.
+
+        Do not try to identify the players or teams, just refer to them as players on offense/defense, etc.
+
+        You do not need to give a frame by frame description, just the most relevant information of the sequence. 
+
+    """
+
+FOUL_JUDGEMENT_PROMPT = """ 
+        You are a seasoned basketball referee with expert knowledge of NBA rules
+        for defensive fouls on a shooter.
+
+        **Your job**
+
+        Given a time-ordered list of annotations of a single basketball play, 
+        decide whether the **primary defender commits a defensive
+        shooting foul** during the shot attempt described.
+
+        Despite being so information dense, these annotations still correspond to a single basketball play taking place over a couple of seconds! it is all one moment to be taken in at once, and the 
+        amount of information is provided in order to give you enough minute information to make your decision: to call foul or not foul.
+
+        <IMPORTANT>
+        You can return free form text, but ensure you surround your final answer with backticks. either ```FOUL``` or ```CLEAN```
+        </IMPORTANT>
+        
+         Look for the following foul indicators _during the shooting motion:
+
+        • Illegal contact with the shooter's arms, wrist, or hand on the ball  
+        • Body-to-body displacement that affects balance or verticality  
+        • Defender invading the shooter's landing space (counter to rule 10-IV-f)  
+        • Contact on the head/neck or airborne shooter (automatic)  
+        • Push from behind or on the side causing altered shot trajectory
+
+
+        annotation data:
+
+        ANNOTATION_DATA_TOKEN"""
+
 class Video:
     def __init__(self, filepath: str):
         self.filepath = filepath
         self.name = os.path.splitext(os.path.basename(self.filepath))[0]
         self.name_no_ext = os.path.splitext(os.path.basename(self.filepath))[0]
+        self.client = LlamaAPIClient(
+            api_key=os.environ.get("LLAMA_API_KEY"),
+        )
+
+
+    # def cut_video(self, start_time, end_time):
+
+
 
     def extract_frames(self, seconds_per_frame=2) -> dict:
 
@@ -62,18 +120,16 @@ class Video:
 
         print(f"Extracted {len(frame_dict)} frames")
         print(f"Saved {frame_count} frames to {frames_dir}")
+        self.frames = frame_dict
         return frame_dict
 
     def describe_frames(self, frames: dict, context: str = None, threads:int = 20) -> dict:
 
-        client = LlamaAPIClient(
-            api_key=os.environ.get("LLAMA_API_KEY"),
-        )
 
         def describe_frame(args):
             seconds, frame = args
             print(f"Describing frame at {seconds} seconds...")
-            response = client.chat.completions.create(
+            response = self.client.chat.completions.create(
             model="Llama-4-Maverick-17B-128E-Instruct-FP8",
             messages=[
                 {
@@ -115,115 +171,83 @@ class Video:
             "-ar", "44100", "-ac", "1", audio_filepath
         ])
 
-def parse_foul_info(v_path):
-    prompt =     """
-        You are a keen eyed basketball referee, watching a basketball play. 
-    
-        Analyze the provided play (represented by a sequence of basketball image frames) for detail relevant for foul calling, especially any contact. If there is contact, you sohuld
-        describe it in detail - what body part was hit, what contact type was used, how severe the contact was. Since the frames comprise a single play, you should interpret the play in the
-        context *all* the provided images. A single image might not provide enough information. Each frame is 50 milliseconds apart!
+    def parse_foul_info(self, time_stamp, boundry_seconds=2):
+        # client = LlamaAPIClient(
+        #     api_key=os.environ.get("LLAMA_API_KEY"),
+        # )
 
-        Your job is not to call fouls, but to describe the action in detail for someone else to call fouls.
+
+
+        v = Video(v_path)
+        frames = v.extract_frames(seconds_per_frame=.1)
+        upperb, lowerb = time_stamp - boundry_seconds, time_stamp + boundry_seconds
+        keys = [k for k in self.frames.keys() if k >= lowerb and k <= upperb]
         
-        We are only concerned with the ball handler/shooter and the primary defender.
+        # keys = list(frames.keys())
+        keys.sort(key=float)
 
-        The overall context of the sequence is known to be a basketball game, so you can focus on the actions of the ball handler and the primary defender.
+        groups = [keys[i:i + 7] for i in range(0, len(keys), 6)]
 
-        Do not try to identify the players or teams, just refer to them as players on offense/defense, etc.
 
-        You do not need to give a frame by frame description, just the most relevant information of the sequence. 
-
-    """
-
-    client = LlamaAPIClient(
-        api_key=os.environ.get("LLAMA_API_KEY"),
-    )
-
-    v = Video(v_path)
-    frames = v.extract_frames(seconds_per_frame=.1)
-
-    keys = list(frames.keys())
-    keys.sort(key=float)
-
-    print(keys)
-    groups = [keys[i:i + 7] for i in range(0, len(keys), 7)]
-    print(groups)
-    analyses = []
-    for group in groups:
-        messages = []
-        messages.append({
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": prompt,
-                },
-            ]
-        })
-        for k in group:
-            frame = frames[k]
+        # print(keys)
+        # print(groups)
+        analyses = []
+        for group in groups:
+            messages = []
             messages.append({
                 "role": "user",
                 "content": [
                     {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": f"data:image/jpeg;base64,{frame['data']}"
-                        },
+                        "type": "text",
+                        "text": DETAIL_ANNOTATOR_PROMPT,
                     },
                 ]
             })
-        resp = client.chat.completions.create(
+            for k in group:
+                frame = frames[k]
+                messages.append({
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{frame['data']}"
+                            },
+                        },
+                    ]
+                })
+            resp = client.chat.completions.create(
+                model="Llama-4-Maverick-17B-128E-Instruct-FP8",
+                messages=messages,
+            )
+            analyses.append(resp.completion_message.content.text)
+
+        # print("\n--------------------------------\n".join(analyses))
+        # print('--------------------------------')
+
+
+        judgement = client.chat.completions.create(
             model="Llama-4-Maverick-17B-128E-Instruct-FP8",
-            messages=messages,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": FOUL_JUDGEMENT_PROMPT.replace("ANNOTATION_DATA_TOKEN", "\n".join(analyses)),
+                        }
+                    ]
+                }
+            ],
         )
-        analyses.append(resp.completion_message.content.text)
 
-    print("\n|||\n".join(analyses))
-
-    judgement_prompt = '''
-        You are a seasoned basketball referee with expert knowledge of NBA rules
-        for defensive fouls on a shooter.
-
-        **Your job**
-
-        Given a time-ordered list of annotations of a single basketball play, 
-        decide whether the **primary defender commits a defensive
-        shooting foul** during the shot attempt described.
-
-        Despite being so information dense, these annotations still correspond to a single basketball play taking place over a couple of seconds! it is all one moment to be taken in at once, and the 
-        amount of information is provided in order to give you enough minute information to make your decision: to call foul or not foul.
-
-         Look for the following foul indicators _during the shooting motion_  
-
-        • Illegal contact with the shooter's arms, wrist, or hand on the ball  
-        • Body-to-body displacement that affects balance or verticality  
-        • Defender invading the shooter's landing space (counter to rule 10-IV-f)  
-        • Contact on the head/neck or airborne shooter (automatic)  
-        • Push from behind or on the side causing altered shot trajectory
-
-
-        annotation data:
-
-        ANNOTATION_DATA_TOKEN
-   '''
-
-    print('--------------------------------')
-    judgement = client.chat.completions.create(
-        model="Llama-4-Maverick-17B-128E-Instruct-FP8",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": judgement_prompt.replace("ANNOTATION_DATA_TOKEN", "\n".join(analyses)),
-                    }
-                ]
-            }
-        ],
-    )
-    print(judgement.completion_message.content.text)
+        judge_text = judgement.completion_message.content.text
+        if "```FOUL```" in judge_text:
+            return True
+        elif "```CLEAN```" in judge_text:
+            return False
+        else:
+            raise ValueError(f"Invalid response!!!!!: {judge_text}")
 
 if __name__ == "__main__":
     
